7ae435c952cce692614edbe59ebcedccf8d2a222
Mario Lisbona <mario.lisbona@gmail.com>
2022-08-04 09:47:56 +1000

    static layout, icons graph and email input

---
+++ script.js
@@ -0,0 +1,26 @@
+const email = document.getElementById('#email');
+const errorMsg = document.querySelector('error-msg');
+const errorContainer = document.querySelector('error-container');
+
+const emailRegex = /\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/;
+
+
+email.oninput = () => {
+    checkEmail(email, emailRegex);
+}
+
+
+
+function checkEmail(email, regex) {
+    if (email.value == '') {
+        // errorContainer.classList.remove('hidden');
+        errorMsg.textContent = 'Email cannot be empty';
+    } else if (!email.value.match(regex)) {
+        errorContainer.classList.remove('hidden');
+        email.style.outline = '1px solid var(light-red)';
+        errorMsg.textContent = 'Please enter a valid email';
+    } else if (email.value.match(regex)) {
+        errorContainer.classList.add('hidden');
+        email.style.outline = '1px solid green';
+    }
+}
