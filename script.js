ecaefc0c76268d7b20124d00b3ce546483419341
Mario Lisbona <mario.lisbona@gmail.com>
2022-08-04 10:53:03 +1000

    email validation and input styling

--- script.js
+++ script.js
@@ -1,26 +1,29 @@
-const email = document.getElementById('#email');
-const errorMsg = document.querySelector('error-msg');
-const errorContainer = document.querySelector('error-container');
+const email = document.getElementById('email');
+const errorMsg = document.querySelector('.error-msg');
+const errorContainer = document.querySelector('.error-container');
 
 const emailRegex = /\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/;
 
 
 email.oninput = () => {
     checkEmail(email, emailRegex);
+
 }
 
 
 
 function checkEmail(email, regex) {
+    console.log(email, regex);
     if (email.value == '') {
-        // errorContainer.classList.remove('hidden');
+        errorContainer.classList.remove('hidden');
+        email.style.border = '1px solid var(--light-red)';
         errorMsg.textContent = 'Email cannot be empty';
     } else if (!email.value.match(regex)) {
         errorContainer.classList.remove('hidden');
-        email.style.outline = '1px solid var(light-red)';
+        email.style.border = '1px solid var(--light-red)';
         errorMsg.textContent = 'Please enter a valid email';
     } else if (email.value.match(regex)) {
         errorContainer.classList.add('hidden');
-        email.style.outline = '1px solid green';
+        email.style.border = '1px solid var(--blue)';
     }
 }
